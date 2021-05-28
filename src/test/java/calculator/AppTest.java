c59e89c9a11006a48f74939569ac2579e29da318
Deepak Lohani <deepak.lohani@skillrary.com>
2021-05-28 08:43:40 +0530

    first commit

---
+++ src/test/java/calculator/AppTest.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+}
