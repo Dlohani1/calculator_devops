a0a47373e1c4c182ed95b8cf0e43d4f712bb4d99
Deepak Lohani <dlohani1@gmail.com>
2022-07-21 11:42:54 +0530

    Updated App.java file

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,7 +12,7 @@ public class App
         int flag=0,ch;
         double num1,num2;
         Scanner reader = new Scanner(System.in);
-        System.out.println("Calculator System");
+        System.out.println("Calculator Application");
         do
         {
             System.out.println("Option Menu");
